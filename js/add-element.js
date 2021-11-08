4cfdd492857ea02fc8efece87dc9511f3bab3f9e
Jared225 <jaredtr@usca.edu>
2021-11-08 12:38:48 -0500

    11/8/2021 version 2

--- js/add-element.js
+++ js/add-element.js
@@ -3,13 +3,14 @@
  */
 
 // get all the hot class elements- change them to cool class
-$(".hot").each(function(){
-  $(this).removeClass("hot");
-  $(this).addClass("cool");
+$('.hot').each(function () {
+  $(this).removeClass('hot');
+  $(this).addClass('cool');
 });
 
 // traverse the elements
-$('#one').next().next().text("milk");
+$('#one').next().next().text('milk');
+$('#one').remove();
 
 // add a new element by clicking the plus sign
 $('#todo').append('<li>candy</li>');
@@ -20,17 +21,26 @@ $('#todo').append('<li>candy</li>');
 $('#add').click(addElement);
 
 function addElement() {
-  if($(this).addElement('#add')){
-    $(this).
-  }
-
+  // if($(this).addElement('#add')){
+  //$(this).
 
   // add a new element
   // add a input text box
-  
+  $('#todo').append('<li><input type ="text"></li>');
+
   // whenever the user are done add the element
-  
+  $('input').blur(function () {
+    $(this).parent().addClass('cool');
+    var items = $(this).val();
+    //replace the input ox by the text
+    $(this).parent().text(items);
+
+    // click the new item will change the style
+    $('li').click(changeStyle);
+  });
 }
+
+//}
 
 // bind click with the event handler
 $('li').click(changeStyle);
@@ -38,18 +48,16 @@ $('li').click(changeStyle);
 //  click the li element will change the changeStyle
 //  three style : complete, cool, hot
 function changeStyle() {
-    if($(this).hasClass('cool')){
-      $(this).removeClass('cool');
-      $(this).addClass('complete');
-    }
-    else if($(this).hasClass('complete')){
-      $(this).removeClass('complete');
-      $(this).addClass('cool');
-    }
-    else {
-      $(this).removeClass('hot');
-      $(this).addClass('complete');
-    }
+  if ($(this).hasClass('cool')) {
+    $(this).removeClass('cool');
+    $(this).addClass('complete');
+  } else if ($(this).hasClass('complete')) {
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  } else {
+    $(this).removeClass('hot');
+    $(this).addClass('complete');
+  }
 }
 
 // delete complete element by clicking the trash can
@@ -57,4 +65,5 @@ document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+  $('li.complete').remove();
 }
